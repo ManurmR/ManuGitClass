b3e7973192065f22e46491490ae6d6af8d73a47b
ManurmR <manurm.renukamohan@gmail.com>
2022-01-29 13:19:53 +0530

    bala done

--- src/org/sample/Employee.java
+++ src/org/sample/Employee.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class Employee {
+	private void m2() {
+		System.out.println("m2 is created by bala");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println("a value is 10:"+a);
